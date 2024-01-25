1e77acca4e6cae089717b97e07c11c89a142aa8c
Хулан Мөнх-Эрдэнэ <HP230061@G303-CC0143.local>
2024-01-25 18:30:12 +0800

    started  signup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,8 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div>Hello World</div>
+    <div className="main-container">
+      <div style={{backgroundColor:'red'}}></div>
+    </div>
   );
 }
---
+++ src/pages/signup/index.tsx
@@ -0,0 +1,7 @@
+export default function SignUp ()  {
+return (
+    <div className="signup-container">
+        <div style={{backgroundColor: 'red', width:'708px'}}></div>
+    </div>
+)
+}
