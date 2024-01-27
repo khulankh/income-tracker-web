58f84b9fb9cea31abddcddfc7c73e6853b1b5df3
Хулан Мөнх-Эрдэнэ <HP230061@G303-CC0143.local>
2024-01-27 12:39:16 +0800

    style n signup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="main-container">
-      <div style={{backgroundColor:'red'}}></div>
+      <div style={{backgroundColor:'red', width:'50vw', height:'100vh'}}></div>
     </div>
   );
 }
--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,7 +1,24 @@
-export default function SignUp ()  {
-return (
-    <div className="signup-container">
-        <div style={{backgroundColor: 'red', width:'708px'}}></div>
-    </div>
-)
+import { Icon } from "@/components/icons/icon";
+
+export default function SignUp() {
+    return (
+        <div className="signup-container" >
+            <div style={{ display: "flex", flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                <Icon />
+                <div style={{ fontSize: '24px' }}>
+                    Create Geld account
+                </div>
+                <div style={{ fontSize: '16px' }}>
+                    Sign up below to create your Wallet account
+                </div>
+                <div className="sign-inputs">
+                    <input id="sign-input"/>
+                    <input id="sign-input"/>
+                    <input id="sign-input"/>
+                    <input id="sign-input"/>
+                </div>
+            </div>
+            <div style={{ backgroundColor: '#0166FF', height: '100vh', width: '50vw' }}></div>
+        </div>
+    )
 }
