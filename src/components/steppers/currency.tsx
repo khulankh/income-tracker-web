f5799714df89ac37782508581d5cb1ecc8f49521
Хулан Мөнх-Эрдэнэ <HP230061@G303-CC0143.local>
2024-03-09 13:53:12 +0800

    file renamed

--- src/components/steppers/currency.tsx
+++ src/components/steppers/currency.tsx
@@ -1,4 +1,4 @@
-import { CurrencyIcon } from "../icons/CurrencyIcon"
+import { MoneyIcon } from "../icons/MoneyIcon"
 const currencyOptions = [
     "MNT - Mongolian Tugrik",
     "USD - United States Dollar",
@@ -19,20 +19,20 @@ const currencyOptions = [
 ]
 export function Currency({ onNext }: { onNext: () => void }) {
     return (
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', position:'absolute', left:'430px'}}>
-                <CurrencyIcon />
-                <h1 style={{ fontSize: '24px', color: '#0F172A', textAlign: 'center' }}>Select base currency</h1>
-                <div style={{ gap: '32px', display: 'flex', flexDirection: 'column' }}>
-                    <div style={{ gap: '12px', display: 'flex', flexDirection: 'column' }}>
-                        <select className="select-option">
-                            {currencyOptions.map((option) => (
-                                <option key={option}>{option}</option>
-                            ))}
-                        </select>
-                        <p style={{ color: '#475569', fontSize: '12px', textAlign: 'center' }}>Your base currency should be the one you use most often. All transaction in other currencies will be calculated based on this one </p>
-                    </div>
-                    <button className="confirm-btn" onClick={onNext}>Confirm</button>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', position: 'absolute', left: '430px' }}>
+            <MoneyIcon />
+            <h1 style={{ fontSize: '24px', color: '#0F172A', textAlign: 'center' }}>Select base currency</h1>
+            <div style={{ gap: '32px', display: 'flex', flexDirection: 'column' }}>
+                <div style={{ gap: '12px', display: 'flex', flexDirection: 'column' }}>
+                    <select className="select-option">
+                        {currencyOptions.map((option) => (
+                            <option key={option}>{option}</option>
+                        ))}
+                    </select>
+                    <p style={{ color: '#475569', fontSize: '12px', textAlign: 'center' }}>Your base currency should be the one you use most often. All transaction in other currencies will be calculated based on this one </p>
                 </div>
+                <button className="confirm-btn" onClick={onNext}>Confirm</button>
             </div>
+        </div>
     )
 }
