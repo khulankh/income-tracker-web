b3a8b48637499f095986e149691a2db3f466d3a4
Хулан Мөнх-Эрдэнэ <HP230061@G303-CC0143.local>
2024-04-19 14:50:27 +0800

    added smth on thursday

--- src/components/dashboard/NavbarComponent.tsx
+++ src/components/dashboard/NavbarComponent.tsx
@@ -12,12 +12,16 @@ export default function NavbarComponent() {
   const handleDashboardButtonClick = () => {
     router.push('/')
   }
+  const handleLogout = () => {
+    localStorage.clear();
+    router.push('/login'); 
+  };
 const toggleLogin = ()=>  router.push('/login');
 
   return (
     <div className="navbar-container">
       <div className="navbar-front">
-        <MainIcon />
+        <MainIcon  />
         <div style={{display:'flex', gap:'8px'}}>
         <button className="navbar-btn" onClick={handleDashboardButtonClick}>Dashboard</button>
         <button className="navbar-btn" onClick={handleRecordsButtonClick}>Records</button>
@@ -28,6 +32,7 @@ const toggleLogin = ()=>  router.push('/login');
        <div onClick={toggleLogin}>
        <Avatar  src="https://s3-alpha-sig.figma.com/img/4b8f/8a06/87e8569e17a69979cf08dac0f798bd37?Expires=1709510400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=PSEkV4v7iQZ1bT6xha8ER-2Kan0HkSA2wiMMmx5M9PzpvYZtE2Ix5gfyobkyW9KwlkCxKpq6EBtATLjHbPrStvnLiLalJytF3vtz7RMIGv86zg5icLyDrIUfpMAGwRkrVThi2IZk8TgC55-83bNW68hQPyhFBnfkwBusr-9NqhBMKVkmHFSIgMYrSlol6foBWhhtvbXQSfAw-QonV3r7BnxHYspicVvoHOVc~H0Tk1CflxFu~Uz4UgoV6xZnJQdK7AJHCdoaP4KLb4EJyIvEMJ1NxREmas8faYLXmStSektIE4MHC3ksjOVu6dtHqKcbUU2pw5Kb-LxaRCy9r~gVsA__"></Avatar>
        </div>
+       <button className="navbar-btn" onClick={handleLogout}>Log Out</button>
       </div>
     </div>
   );
